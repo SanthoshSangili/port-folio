d8a132048f3b8c25874054e0c742f360bf1fd10c
Santhosh <santhoshsangili.2802@gmail.com>
2025-12-11 17:34:44 +0530

    Latest Commit

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 
-// var domaiUrl = 'https://port-folio-api.onrender.com/';
-var domaiUrl = 'http://localhost:8080/';
+var domaiUrl = 'https://port-folio-api.onrender.com/';
+// var domaiUrl = 'http://localhost:8080/';
 
 export const environment = {
   geminiAiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
